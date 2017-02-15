b9a39e8381f3467194771ab8b6c2ee2286c02348
Deepthi Sreerama <ds742@afsconnect1.njit.edu>
2017-02-14 23:20:19 -0500

    Calculated discount amount and price

--- display_discount.php
+++ display_discount.php
@@ -3,6 +3,11 @@
 	$product_description = filter_input(INPUT_POST, 'product_description');
 	$list_price = filter_input(INPUT_POST, 'list_price');
 	$discount_percent = filter_input(INPUT_POST, 'discount_percent');
+
+        $discount = $list_price * $discount_percent * .01;
+        $discount_price = $list_price - $discount;
+        
+        
  ?>
 <!DOCTYPE html>
 <html>
@@ -23,11 +28,11 @@
         <label>Standard Discount:</label>
         <span><?php echo htmlspecialchars($discount_percent); ?></span><br>
 
-        <label>Discount Amount:</label>
-        <span><?php echo ''; ?></span><br>
+         <label>Discount Amount:</label>
+        <span><?php echo $discount; ?></span><br>
 
         <label>Discount Price:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $discount_price; ?></span><br>
     </main>
 </body>
 </html>
