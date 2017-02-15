e018e43315f0dbcb2ea09a80ea34c4f84645796c
Deepthi Sreerama <ds742@afsconnect1.njit.edu>
2017-02-14 22:53:25 -0500

    Added post array

---
+++ display_discount.php
@@ -0,0 +1,33 @@
+<?php
+
+	$product_description = filter_input(INPUT_POST, 'product_description');
+	$list_price = filter_input(INPUT_POST, 'list_price');
+	$discount_percent = filter_input(INPUT_POST, 'discount_percent');
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Product Discount Calculator</title>
+    <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+    <main>
+        <h1>This page is under construction</h1>
+
+        <label>Product Description:</label>
+        <span><?php echo ''; ?></span><br>
+
+        <label>List Price:</label>
+        <span><?php echo ''; ?></span><br>
+
+        <label>Standard Discount:</label>
+        <span><?php echo ''; ?></span><br>
+
+        <label>Discount Amount:</label>
+        <span><?php echo ''; ?></span><br>
+
+        <label>Discount Price:</label>
+        <span><?php echo ''; ?></span><br>
+    </main>
+</body>
+</html>
